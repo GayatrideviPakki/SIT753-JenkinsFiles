aab9d20066fd9ccf76f63319f15b5793d3cd508b
Gayatri <gaya3devi2@gmail.com>
2023-05-04 14:45:59 +1000

    significant changes

--- Jenkinspipeline2.groovy
+++ Jenkinspipeline2.groovy
@@ -2,60 +2,66 @@ pipeline{
     agent any
     environment {
         DIRECTORY_PATH="C:/Program Files/Jenkins"
-        TESTING_ENVIRONMENT="QA"
-        PRODUCTION_ENVIRONMENT="GAYATRI"
+        TESTING_ENVIRONMENT="CCS_ICT_UAT"
+        PRODUCTION_ENVIRONMENT="CCS_ICT_PROD"
     }
     stages{
         stage('Build'){
             steps{
                 echo "fetching the source code from the directory: $DIRECTORY_PATH "
-                echo "compile the code and generate any necessary artifacts ......"
+                echo "compile the code and Build the code using [Maven] to compile and package code. ......"
             }
         }
         stage('Unit and Integration Tests'){
             steps{
-                echo "unit test is running in this stage.. "
-                echo "integration tests are running ....! "
+                echo "unit test is running in this stage from [Buildkite].. "
+                echo "integration tests are running from [Buildkite]....! "
+                echo "Unit and Integration Tests completed ...."
             }
         }
         stage('Code Analysis'){
             steps{
-                echo "Analyse Code...."
+                echo "Static Code quality analysis being performed using [SonarQube]...."
             }
         }
         stage('Security Scan'){
             steps{
-                echo "Security Scan on: $TESTING_ENVIRONMENT"
+                echo "Security Scan started by [SonarQube] ..."
             }
             post{
                 success{
                     emailext attachLog: true,
                     compressLog: true,
                     to: 'gayatridevikagapu@gmail.com',
-                    body: 'Security Scan Success and log at ${env.BUILD_URL}/consoleText',
-                    subject: 'Jenkins Email notification'
-                }
+                    body: 'Security Scan Successfule. please find more details in the log file',
+                    subject: 'Security Scan Success - Jenkins'
                 failure {  
-                    mail to: "gayatridevikagapu@gmail.com",
-                    subject: "Test Jenkins Email",
+                    emailext to: "gayatridevikagapu@gmail.com",
+                    subject: "Test Jenkins Email - Jenkins",
                     body: "Security Scan Failure"  
                 }
+            }
+            echo "Security Scan ended. Check the log file for more details"
+        }
+        stage('Deploy to Staging'){
+            steps{
+                echo "Deploying code into Test environment: $TESTING_ENVIRONMENT"
             }
         }
         stage('Integration Tests on Staging'){
             steps{
-                echo "Integration tests stage.."
+                echo "Running Integration tests at this stage.."
                 sleep 5
             }
             post{
                 success{
-                    mail to: "gayatridevikagapu@gmail.com",
-                    subject: "Test Jenkins Email",
+                    emailext to: "gayatridevikagapu@gmail.com",
+                    subject: "Test Jenkins Email - Jenkins",
                     body: "success"
                 }
                 failure {  
-                    mail to: "gayatridevikagapu@gmail.com",
-                    subject: "Test Jenkins Email",
+                    emailext to: "gayatridevikagapu@gmail.com",
+                    subject: "Test Jenkins Email - Jenkins",
                     body: "Failure"  
                 } 
             }
@@ -65,7 +71,21 @@ pipeline{
             steps{
                 echo "Deploy to Production environment:  $PRODUCTION_ENVIRONMENT "
             }
-            
+             post{
+                success{
+                    emailext attachLog: true,
+                    compressLog: true,
+                    to: 'gayatridevikagapu@gmail.com',
+                    body: 'Deploy to Production environment is finished and build log is available at $JENKINS_HOME/jobs/$JOB_NAME/builds/lastSuccessfulBuild/log',
+                    subject: 'Deployment to Production Failed is Success - Jenkins'
+                }
+                failure {  
+                    emailext attachLog: true,
+                    compressLog: true,
+                    to: 'gayatridevikagapu@gmail.com',
+                    body: 'Deploy to Production environment is finished and build log is available at $JENKINS_HOME/jobs/$JOB_NAME/builds/lastSuccessfulBuild/log',
+                    subject: "Deployment to Production Failed - Jenkins"  
+                }
         }
     }
 }
